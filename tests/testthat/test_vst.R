9be050f3076918c8d949b428229e43c2853cddc3
Christoph Hafemeister <chafemeister@nygenome.org>
2018-11-16 11:35:40 +0100

    Update tests

--- tests/testthat/test_vst.R
+++ tests/testthat/test_vst.R
@@ -5,8 +5,8 @@ test_that('vst runs and returns expected output', {
   options(mc.cores = 2)
   set.seed(42)
   vst_out <- vst(pbmc, return_gene_attr = TRUE, return_cell_attr = TRUE)
-  expect_equal(c(9727, 402), dim(vst_out$y))
+  expect_equal(c(910, 283), dim(vst_out$y))
   ga <- vst_out$gene_attr[order(-vst_out$gene_attr$residual_variance), ]
-  expect_equal(c("PPBP", "C4orf3", "MTRF1", "VDAC3", "PF4"), rownames(ga)[1:5])
-  expect_equal(c(201.09918, 150.19898, 92.17449, 74.05857, 70.59825), ga$residual_variance[1:5], tolerance = 1e-05)
+  expect_equal(c("GNLY", "PPBP", "PF4", "GNG11", "SDPR"), rownames(ga)[1:5])
+  expect_equal(c(14.47994, 12.90632, 12.35566, 11.25633, 10.10952), ga$residual_variance[1:5], tolerance = 1e-05)
 })
